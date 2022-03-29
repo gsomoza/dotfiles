edabf42680a259bd1664ea64f432f801fa865548
Gabriel Somoza <gabriel@strategery.io>
2022-03-29 13:40:22 +0200

    Fix brew installation

--- setup.zsh
+++ setup.zsh
@@ -3,7 +3,7 @@
 DOTFILES_DIR="$HOME/dotfiles"
 
 function installHomebrew() {
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew update
     brew install git
 }
